158f1388669f87aaca572530db3bf755b7b43338
Zubairsaififr <juberjubersaifi@gmail.com>
2026-01-01 18:21:11 +0530

    b

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './About.css'
 import Card from '../Card/Card'
-import python from "../../assets/python.png"
-import react from "../../assets/reacttt.png"
-import mysql from "../../assets/mysql.png"
+// import python from "../../assets/pay.png"
+// import react from "../../assets/multi.png"
+// import mysql from "../../assets/hrpolo.png"
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
@@ -13,92 +13,107 @@ import { ScrollTrigger } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger)
 function About() {
-  useGSAP(()=>{
-    gsap.from(".circle ",{
+  useGSAP(() => {
+    gsap.from(".circle ", {
       x: -100,
       opacity: 0,
       duration: 1,
-      sragger:1,
-      scrollTrigger:{
+      sragger: 1,
+      scrollTrigger: {
         trigger: ".circle",
         scroll: "body",
         scrub: 2,
-     
+
         start: "top 60%",
         end: "top 30%",
-        
+
       }
     })
-     gsap.from(".line ",{
+    gsap.from(".circle ", {
       x: -100,
       opacity: 0,
       duration: 1,
-      stagger:1,
-      scrollTrigger:{
+      sragger: 1,
+      scrollTrigger: {
+        trigger: ".circle",
+        scroll: "body",
+        scrub: 2,
+
+        start: "top 60%",
+        end: "top 30%",
+
+      }
+    })
+    gsap.from(".line ", {
+      x: -100,
+      opacity: 0,
+      duration: 1,
+      stagger: 1,
+      scrollTrigger: {
         trigger: ".line",
         scroll: "body",
         scrub: 2,
-        
+
         start: "top 60%",
         end: "top 30%",
-        
+
       }
     })
 
-gsap.from(".aboutdetails h1 ",{
+    gsap.from(".aboutdetails h1 ", {
       x: -100,
       opacity: 0,
       duration: 1,
-      stagger:1,
-      scrollTrigger:{
+      stagger: 1,
+      scrollTrigger: {
         trigger: ".aboutdetails h1",
         scroll: "body",
         scrub: 2,
-        
+
         start: "top 60%",
         end: "top 30%",
-        
+
       }
     })
-    gsap.from(".aboutdetails ul",{
+    gsap.from(".aboutdetails ul", {
       y: 100,
       opacity: 0,
       duration: 1,
-      stagger:1,
-      scrollTrigger:{
+      stagger: 1,
+      scrollTrigger: {
         trigger: ".aboutdetails ul",
         scroll: "body",
         scrub: 2,
-      
+
         start: "top 60%",
         end: "top 30%",
-        
+
       }
     })
-    gsap.from(".rightabout ",{
+    gsap.from(".rightabout ", {
       y: 100,
       opacity: 0,
       duration: 1,
-      stagger:1,
-      scrollTrigger:{
+      stagger: 1,
+      scrollTrigger: {
         trigger: ".rightabout",
         scroll: "body",
         scrub: 2,
-       
+
         start: "top 60%",
         end: "top 30%",
-        
+
       }
     })
 
 
   })
-  useGSAP(()=>{
-    gsap.from(".card",{
+  useGSAP(() => {
+    gsap.from(".card", {
       x: -100,
       opacity: 0,
       duration: 1,
-      scrollTrigger:{
+      scrollTrigger: {
         trigger: ".card",
         scroll: "body",
         scrub: 2,
@@ -107,70 +122,91 @@ gsap.from(".aboutdetails h1 ",{
 
     })
 
-      
-  })
-  
-  return (
-    <div id='about'>
-      <div className="leftabout">
-        <div className="circle-line">
-          <div className="circle"></div>
-          <div className="line"></div>
-          <div className="circle"></div>
-          <div className="line"></div>
-           <div className="circle"></div>
-           <div className="line"></div>
-           <div className="circle"></div>
-        </div>
-        <div className="aboutdetails">
-          
-          <div className="Education">
-            <h1>Education</h1>
-            <ul>
-              <li><span>Degree</span>: B-Tech (Mechanical Engineering)</li>
-              <li><span>Intermediate </span>: UP Board (2018) </li>
-              <li><span>High School</span>:UP Board (2016) </li>
-              
-            </ul>
-          </div>
-          <div className="Certification">
-            <h1>Certification</h1>
-            <ul>
-              <li>Python Fullstack Web Development <span>Ducat Sector 63 Noida</span></li>
-              <li>AR/VR Development By IIT Mundi <span>Ducat Sector 63 Noida</span></li>
-              <li>Solid Work <span> Cadd Center Ghaziabad</span></li>
-              
-            </ul>
-          </div>
-          <div className="Skills">
-            <h1>Skills</h1>
-            <ul>
-              <li>Python Fullstack </li>
-              <li>Web Development</li>
-              <li>React.js</li>
-              <li>MySql</li>
-              
-            </ul>
-          </div>
-          <div className="personalinfo">
-            <h1>Personal Information</h1>
-            <ul>
-              <li><span>Name</span>: Zubair Saifi</li>
-              <li><span>D.O.B</span>: 25-June-2000</li>
-              <li><span>Gender</span>: Male</li>
-              <li><span>Language Known</span>: English, Hindi</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="rightabout">
-        <Card title="Python" image={python}/>
-        <Card title="React.js" image={react}/>
-        <Card title="MySql" image={mysql}/>
-      </div>
 
-        
-    </div>
+  })
+
+  return (
+    <>
+      <div id="sum" className='ppp'>
+
+        <div className='AZU'>
+          <h1>Professional Summary </h1>
+          <h3>Mechanical Design Engineer with over 3.5 years of hands-on experience in the lithium-ion battery industry, specializing in the design and development of battery packs, modules, and enclosures for Electric Vehicles (EV), Home UPS systems, and Energy Storage Systems (ESS/BESS).
+            Proficient in SolidWorks for creating robust, production-ready 3D models and detailed drawings, with strong expertise in sheet metal design, IP-rated enclosures for environmental protection, and adherence to Design for Manufacturability (DFM) and Design for Assembly (DFA) principles.
+            Skilled in generating comprehensive manufacturing documentation, including GD&T, BOMs, and assembly instructions, to ensure seamless transition from concept to production.
+            Proven track record of collaborating with cross-functional teams— including electrical, thermal, and manufacturing engineers—to deliver safe, reliable, cost-optimized, and high-performance battery solutions that meet stringent industry standards.
+            Passionate about advancing sustainable energy technologies through innovative mechanical design.</h3>
+        </div>
+        <div id='about'>
+
+          <div className="leftabout">
+            <div className="circle-line">
+              <div className="circle"></div>
+              <div className="line"></div>
+              <div className="circle"></div>
+              <div className="line"></div>
+              <div className="circle"></div>
+              <div className="line"></div>
+              <div className="circle"></div>
+            </div>
+            <div className="aboutdetails">
+
+              <div className="Education">
+                <h1>Education</h1>
+                <ul>
+                  <li><span>B-Tech</span>: Mechanical Engineering (2018-2022)</li>
+                  <li>Hi-Tech Institute of Engineering & Technology, AKTU University   </li>
+                  {/* <li><span>Graduation</span>: Bachelor's In Business Administration  (2021) </li>
+                  <li>CCS University  </li> */}
+
+                </ul>
+              </div>
+              {/* <div className="Certification">
+                <h1>Certification</h1>
+                <ul>
+                  <li> <span>Ducat Sector 63 Noida</span></li>
+                  <li>AR/VR Development By IIT Mundi <span>Ducat Sector 63 Noida</span></li>
+                  <li>Solid Work <span> Cadd Center Ghaziabad</span></li>
+
+                </ul>
+              </div> */}
+              <div className="Skills">
+                <h1>Skills</h1>
+                <ul>
+                  <li>Administration </li>
+                  <li>Recruitment </li>
+                  <li>Scanning and Sourcing</li>
+                  <li>Maintaining KPIs </li>
+                  <li>Staffing and Performance Management  </li>
+                  <li>Time management  </li>
+                  <li>Salary Negotiation </li>
+                  <li>Salary Negotiation </li>
+                  <li>Salary Negotiation </li>
+
+                </ul>
+              </div>
+              <div className="personalinfo">
+                <h1>Personal Information</h1>
+                <ul>
+                  <li><span>Name</span>: Azuka Gazi</li>
+                  <li><span>D.O.B</span>: 14-Dec-2000</li>
+                  <li><span>Gender</span>: Female</li>
+                  <li><span>Language Known</span>: English, Hindi,Urdu</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="rightabout">
+            {/* <Card title="Payroll Management" image={python} />
+            <Card title="Multitasking" image={react} />
+            <Card title="Multitasking" image={react} />
+            <Card title="HR Policies" image={mysql} /> */}
+          </div>
+
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,7 +32,7 @@ tl.from("nav ul li",{
     <h1>PORTFOLIO</h1>
     <ul className='desktopmenu'>
        <Link to="home" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Home</li></Link>
-       <Link to="about" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
+       <Link to="sum" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
        <Link to="projects" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Projects</li></Link>
        <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Contact</li></Link>
        
@@ -47,7 +47,7 @@ tl.from("nav ul li",{
     </div>
     <ul className='mobilemenu' ref={mobile}>
        <Link to="home" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Home</li></Link>
-       <Link to="about" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
+       <Link to="sum" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
        <Link to="projects" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Projects</li></Link>
        <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Contact</li></Link>
        
